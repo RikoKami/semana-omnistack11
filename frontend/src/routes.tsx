e011dcf944772fbd0ec4bcd430bcb8e2e03b2a3c
Larissa <orglary@gmail.com>
2020-03-29 02:37:27 -0300

    page profile

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Logon from './containers/Logon';
 import Register from './containers/Register';
+import Profile from './containers/Profile';
 
 const Routes = () => {
     return(
@@ -10,6 +11,7 @@ const Routes = () => {
             <Switch>
                 <Route path="/" exact component={Logon} />
                 <Route path="/register" component={Register} />
+                <Route path="/profile" component={Profile} />
             </Switch>
         </BrowserRouter>
     )
