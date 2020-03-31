106cfd7e6cf1f9ec3eab6474dc1fc32e22934955
Larissa <orglary@gmail.com>
2020-03-31 02:21:48 -0300

    add validation with celebrate

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors')
+const { errors } = require('celebrate');
 const routes = require('./routes');
 
 const app = express();
@@ -8,6 +9,7 @@ app.use(cors({
     // origin: 'http://dominio.com'
 }));
 app.use(express.json());
-app.use(routes)
+app.use(routes);
+app.use(errors());
 
 app.listen(3333);
