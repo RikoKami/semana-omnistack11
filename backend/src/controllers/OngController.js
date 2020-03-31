be8deed3c2773790ec61c38444e3f751e0401ef4
Larissa <orglary@gmail.com>
2020-03-31 03:16:23 -0300

    add test generate unique id for ONG with Jest in backend

--- backend/src/controllers/OngController.js
+++ backend/src/controllers/OngController.js
@@ -1,11 +1,11 @@
-const crypto = require('crypto');
+const generateUniqueId = require('../utils/generateUniqueId')
 /* Conexão com o banco de dados */
 const connection = require('../database/connections');
 
 module.exports = {
     async create(req,res) {
         const { name, email, whatsapp, city, uf } = req.body;
-        const id = crypto.randomBytes(4).toString('HEX');
+        const id = generateUniqueId() 
     
         await connection('ongs').insert({
             id,
